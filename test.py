10b1d03b03e6b2da9cd61af01f2e2c177125c6b2
Tao Chenyue <3038816978@qq.com>
2022-01-20 21:20:33 +0800

    添加了比对参考，在test.py

--- test.py
+++ test.py
@@ -1,13 +1,32 @@
-﻿#import openpyxl 
+﻿import openpyxl
+from openpyxl.styles import PatternFill
+from main import minDistance
 
-#workbook_read = openpyxl.load_workbook('2.4.0成就_CHS.xlsx')
-#worksheet_read = workbook_read['Full']
-#max_row=worksheet_read.max_row
-    
-#achievements=[]
-#for i in range(1,max_row):
-#    achievements.append(worksheet_read['B'+str(i+1)].value)
+def Search(a,list):
+    for i,x in enumerate(list):
+        if minDistance(a,x)<3:
+            return i
+    return -1
 
-from main import Find 
+workbook_read = openpyxl.load_workbook('test.xlsx')
+worksheet_read = workbook_read['Full']
+max_row=worksheet_read.max_row
 
-print(Find('白之光'))
+my_achievements=[]
+my_situation=[]
+for i in range(1,max_row):
+    my_achievements.append(worksheet_read['c'+str(i+1)].value)
+    my_situation.append('达成' if not '/' in worksheet_read['e'+str(i+1)].value else worksheet_read['e'+str(i+1)].value)
+workbook_read = openpyxl.load_workbook('2.4.0成就_CHS.xlsx')
+worksheet_read = workbook_read['Full']
+max_row=worksheet_read.max_row
+fill = PatternFill(start_color ='FFFF00', end_color = 'FFFF00', fill_type = 'solid')
+
+for i in range(1,max_row):
+    achievement=worksheet_read['b'+str(i+1)].value
+    index=Search(achievement, my_achievements)
+    if index==-1 or (index!=-1 and my_situation[index]!='达成'):
+        worksheet_read['b'+str(i+1)].fill=fill 
+
+workbook_read.save('compare_ans.xlsx')
+
